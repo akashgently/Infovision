2dd4a9df70231af495525ba50bb307881c8ce690
akashgently <102671235+akashgently@users.noreply.github.com>
2025-06-11 12:23:09 +0530

    Banking Application

---
+++ src/com/Assignment-1/AccountOperations.java
@@ -0,0 +1,6 @@
+package com.assignment1;
+
+public interface AccountOperations {
+	 void openAccount();
+	 void displayAccountDetails();
+}
---
+++ src/com/Assignment-1/BankAccount.java
@@ -0,0 +1,8 @@
+package com.assignment1;
+
+public abstract class BankAccount implements AccountOperations {
+	public String account_type;
+	 public String accountHolderName;
+	 public String aadhar_number;
+	 public String pan_number;
+	}
