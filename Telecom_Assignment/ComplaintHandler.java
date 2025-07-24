2e2bd6838025616cfd1ac2865b53dbe9f444f121
akashgently <102671235+akashgently@users.noreply.github.com>
2025-07-24 20:21:56 +0530

    Delete Telecom_Assignment directory

--- Telecom_Assignment/ComplaintHandler.java
+++
@@ -1,7 +0,0 @@
-package com.Telecom_Assignment;
-
-public interface ComplaintHandler {
-    void fileComplaint(String description);
-    void viewComplaints();
-}
-
--- Telecom_Assignment/ServiceSubscriber.java
+++
@@ -1,6 +0,0 @@
-package com.Telecom_Assignment;
-
-public interface ServiceSubscriber {
-    void subscribeVAS(String service);
-    void unsubscribeVAS(String service);
-}
