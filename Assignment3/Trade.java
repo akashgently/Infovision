dde593172f732e3bad9ad25813301ff4f36c885e
akashgently <102671235+akashgently@users.noreply.github.com>
2025-07-24 20:22:19 +0530

    Delete Assignment3 directory

--- Assignment3/Trade.java
+++
@@ -1,7 +0,0 @@
-package com.Assignment3;
-
-public interface Trade {
-	void buyStock(String stockId, int quantity) throws StockNotFoundException, InsufficientMarketSharesException;
-	
-	void sellStock(String stockId, int quantity) throws StockNotFoundException,InsufficientSharesException;
-}
