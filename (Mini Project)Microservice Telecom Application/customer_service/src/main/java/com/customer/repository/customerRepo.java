4794d1fb5166cce68d5332204e2afa9a44a1312d
akashgently <102671235+akashgently@users.noreply.github.com>
2025-07-24 20:20:37 +0530

    Delete (Mini Project)Microservice Telecom Application directory

--- (Mini Project)Microservice Telecom Application/customer_service/src/main/java/com/customer/repository/customerRepo.java
+++
@@ -1,7 +0,0 @@
-package com.customer.repository;
-
-import com.customer.entity.Customer;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface customerRepo extends JpaRepository<Customer,Long> {
-}
--- (Mini Project)Microservice Telecom Application/plan_service/src/main/java/com/plan/repository/planRepository.java
+++
@@ -1,9 +0,0 @@
-package com.plan.repository;
-
-import com.plan.entity.Plan;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface planRepository extends JpaRepository<Plan,Long>
-{
-
-}
