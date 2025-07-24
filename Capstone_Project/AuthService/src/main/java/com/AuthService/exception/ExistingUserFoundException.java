ec844bf61dfd2375321a646bd692bcea187b9caf
akashgently <102671235+akashgently@users.noreply.github.com>
2025-07-24 20:22:08 +0530

    Delete Capstone_Project directory

--- Capstone_Project/AuthService/src/main/java/com/AuthService/exception/ExistingUserFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.AuthService.exception;
-
-public class ExistingUserFoundException extends Exception{
-    public ExistingUserFoundException(String message) {
-        super(message);
-    }
-}
--- Capstone_Project/AuthService/src/main/java/com/AuthService/exception/MissingFieldException.java
+++
@@ -1,7 +0,0 @@
-package com.AuthService.exception;
-
-public class MissingFieldException extends Exception{
-    public MissingFieldException(String message) {
-        super(message);
-    }
-}
